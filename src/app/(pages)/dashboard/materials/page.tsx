610775ad94da7cbac724291ef35dee0764549e2e
Maurício Miranda <mauriciomirandagomes@gmail.com>
2024-12-26 09:04:14 -0300

    fix: updt nextJS e dependências

--- src/app/(pages)/dashboard/materials/page.tsx
+++ src/app/(pages)/dashboard/materials/page.tsx
@@ -11,9 +11,8 @@ interface Material {
 
 const Materials: React.FC = () => {
   const [materials, setMaterials] = useState<Material[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     const fetchMaterials = async () => {
       try {
         const response = await fetch("/api/materials");
@@ -27,11 +26,11 @@ const Materials: React.FC = () => {
     };
 
     fetchMaterials();
-  }, []);
+  }, []); */
 
-  if (loading) {
+  /*   if (!materials) {
     return <div>Loading...</div>;
-  }
+  } */
 
   return (
     <div className="flex flex-col items-center w-full h-screen bg-white pt-10">
@@ -42,15 +41,26 @@ const Materials: React.FC = () => {
         <FaArrowLeft className="mr-1" />
         Voltar
       </Link>
-      <h1>Download Materials</h1>
+      <h1 className="font-destaque-gg text-destaque-gg pt-8 pb-2 text-verde-escuro">
+        Meus materiais
+      </h1>
       <ul>
-        {materials.map((material) => (
-          <li key={material.id}>
-            <a href={material.url} download>
-              {material.title}
-            </a>
-          </li>
-        ))}
+        {materials.length ? (
+          materials.map((material) => (
+            <li key={material.id}>
+              <a href={material.url} download>
+                {material.title}
+              </a>
+            </li>
+          ))
+        ) : (
+          <div className="flex flex-col items-center py-10 px-3 text-pessego">
+            <h2 className="font-titulos text-5xl pb-3 text-center">Poxa!</h2>
+            <p className="font-subtitulos text-subtitulos text-center">
+              Ainda não temos materiais disponíveis para download.
+            </p>
+          </div>
+        )}
       </ul>
     </div>
   );
